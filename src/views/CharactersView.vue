21c47afec05f19e6770efeb6530d93a2666e3673
Nekonyx13 <corvin.prath@gmail.com>
2024-03-28 20:17:08 +0100

    Add CharactersView and implement zoom functionality

---
+++ src/views/CharactersView.vue
@@ -0,0 +1,62 @@
+<script setup lang="ts">
+import { onMounted, ref } from 'vue'
+
+const toolbarData = {
+  mode: 'select',
+  zoom: 1
+}
+
+const zoomValue = ref(toolbarData.zoom)
+
+const contentWindow = ref<HTMLElement | null>(null)
+const content = ref<HTMLElement | null>(null)
+
+function handleZoom(e: WheelEvent) {
+  if (!e.ctrlKey) return
+  e.preventDefault()
+  if (e.deltaY > 0) {
+    zoomValue.value -= Math.min(0.1, Math.pow(zoomValue.value, 2) * 0.1)
+  } else {
+    zoomValue.value += 0.1
+  }
+}
+</script>
+
+<template>
+  <div
+    ref="content-window"
+    class="w-full h-full bg-gray-400 overflow-auto relative"
+    @wheel="handleZoom"
+  >
+    <!-- Toolbar -->
+    <div
+      class="toolbar flex flex-col gap-2 bg-accent/20 rounded p-2 absolute z-10 top-1/2 transform -translate-y-1/2"
+    >
+      <button @click="zoomValue = 1">R</button>
+      <button @click="zoomValue += 0.4">+</button>
+      <button @click="zoomValue -= 0.4">-</button>
+      <span> {{ zoomValue }} </span>
+    </div>
+
+    <div
+      ref="content"
+      :style="{
+        transform: `scale(${zoomValue})`,
+        transformOrigin: 'top left',
+        transition: 'transform 0.1s ease-out'
+      }"
+      class="bg-gray-300"
+    >
+      <p class="text-lg">Zoom me!</p>
+      <input v-model="zoomValue" type="range" min="0.5" max="2" step="0.1" class="" />
+      <input type="text" class="w-20" />
+      <textarea class="w-20 h-20"></textarea>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.toolbar button {
+  @apply w-8 h-8 flex items-center justify-center rounded bg-accent text-white;
+}
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,4 +1,16 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { onMounted, ref } from 'vue'
+
+const zoomValue = ref(1)
+
+onMounted(() => {
+  // on mouse wheel event zoom in/out
+  window.addEventListener('wheel', (e) => {
+    e.preventDefault()
+    zoomValue.value += e.deltaY > 0 ? -0.1 : 0.1
+  })
+})
+</script>
 
 <template>
   <h1 class="">Hello World!</h1>
@@ -7,4 +19,16 @@
     Lorem ipsum dolor sit amet, consectetur adipiscing elit sed do eiusmod tempor incididunt ut
     labore et dolore magna aliqua.
   </p>
+
+  <div class="w-[600px] h-[400px] bg-gray-200 rounded-lg shadow-lg overflow-auto">
+    <div
+      :style="{ transform: `scale(${zoomValue})`, transformOrigin: 'top left' }"
+      class="bg-gray-300"
+    >
+      <p class="text-lg">Zoom me!</p>
+      <input v-model="zoomValue" type="range" min="0.5" max="2" step="0.1" class="" />
+      <input type="text" class="w-20" />
+      <textarea class="w-20 h-20"></textarea>
+    </div>
+  </div>
 </template>
